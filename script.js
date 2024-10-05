cda26c866847af1fd7e9ad08c2f248d8dd09d36a
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-05 09:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ let counter=0;
+
+        function increment(){
+            const previousValue =counter;
+            counter++;
+            document.getElementById("counter").innerHTML=counter;
+            alert(previousValue);
+        }
